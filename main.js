c848856db84762cce57977247f83bfbd6d02c482
MV <mvntando@gmail.com>
2024-12-29 19:46:28 +0200

    initialize Vue and Electron project with basic structure and configuration

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 1300,
+        height: 700,
+        frame: false,  // Removes the default title bar
+        webPreferences: {
+        preload: path.join(__dirname, 'preload.js'), // Optional if using preload scripts
+        contextIsolation: true, // Recommended for security
+        nodeIntegration: false, // Avoid nodeIntegration for security
+        },
+    });
+
+    // Uncomment this to load production files instead
+    // mainWindow.loadFile(path.join(__dirname, 'view/dist/index.html'));
+
+    // Load Vue's development server in development
+    mainWindow.loadURL('http://localhost:8080');
+
+});
+
+// Quit the app when all windows are closed, except on macOS
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
